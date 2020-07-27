eb91cf9167cc43b80719f039c69d43aa7f2ed72d
Satrya <margasatrya@gmail.com>
2020-07-27 09:54:56 +0700

    Update package

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,13 @@
-module.exports = function( grunt ) {
+module.exports = function (grunt) {
 
 	// Load all Grunt tasks
-	require( 'jit-grunt' )( grunt, {
+	require('jit-grunt')(grunt, {
 		makepot: 'grunt-wp-i18n'
-	} );
+	});
 
-	grunt.initConfig( {
+	grunt.initConfig({
 
-		pkg: grunt.file.readJSON( 'package.json' ),
+		pkg: grunt.file.readJSON('package.json'),
 
 		// Copy the theme into the build directory
 		copy: {
@@ -47,7 +47,7 @@ module.exports = function( grunt ) {
 				},
 				expand: true,
 				cwd: 'build/<%= pkg.name %>/',
-				src: [ '**/*' ],
+				src: ['**/*'],
 				dest: '<%= pkg.name %>/'
 			}
 		},
@@ -67,35 +67,35 @@ module.exports = function( grunt ) {
 					potFilename: '<%= pkg.name %>.pot', // Name of the POT file.
 					type: 'wp-plugin', // Type of project (wp-plugin or wp-theme).
 					updateTimestamp: true, // Whether the POT-Creation-Date should be updated without other changes.
-					processPot: function( pot, options ) {
-						pot.headers[ 'report-msgid-bugs-to' ] = 'https://idenovasi.com/contact/';
-						pot.headers[ 'plural-forms' ] = 'nplurals=2; plural=n != 1;';
-						pot.headers[ 'last-translator' ] = 'Idenovasi\n';
-						pot.headers[ 'language-team' ] = 'Idenovasi\n';
-						pot.headers[ 'x-poedit-basepath' ] = '..\n';
-						pot.headers[ 'x-poedit-language' ] = 'English\n';
-						pot.headers[ 'x-poedit-country' ] = 'UNITED STATES\n';
-						pot.headers[ 'x-poedit-sourcecharset' ] = 'utf-8\n';
-						pot.headers[ 'x-poedit-searchpath-0' ] = '.\n';
-						pot.headers[ 'x-poedit-keywordslist' ] = '__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n';
-						pot.headers[ 'x-textdomain-support' ] = 'yes\n';
+					processPot: function (pot, options) {
+						pot.headers['report-msgid-bugs-to'] = 'https://idenovasi.com/contact/';
+						pot.headers['plural-forms'] = 'nplurals=2; plural=n != 1;';
+						pot.headers['last-translator'] = 'Idenovasi\n';
+						pot.headers['language-team'] = 'Idenovasi\n';
+						pot.headers['x-poedit-basepath'] = '..\n';
+						pot.headers['x-poedit-language'] = 'English\n';
+						pot.headers['x-poedit-country'] = 'UNITED STATES\n';
+						pot.headers['x-poedit-sourcecharset'] = 'utf-8\n';
+						pot.headers['x-poedit-searchpath-0'] = '.\n';
+						pot.headers['x-poedit-keywordslist'] = '__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n';
+						pot.headers['x-textdomain-support'] = 'yes\n';
 						return pot;
 					}
 				}
 			}
 		},
 
-	} );
+	});
 
 	// Production task
-	grunt.registerTask( 'build', [
+	grunt.registerTask('build', [
 		'makepot',
 		'copy'
-	] );
+	]);
 
 	// Production task
-	grunt.registerTask( 'package', [
+	grunt.registerTask('package', [
 		'compress'
-	] );
+	]);
 
 };
